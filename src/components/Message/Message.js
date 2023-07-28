7a2b84bfa439152e59941c68e16d335c655c0b72
TalSB <artishok.tal@gmail.com>
2023-07-28 19:09:10 +0300

    own and other message designs

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -2,9 +2,15 @@ import React from 'react';
 import styles from './message.module.css';
 
 export function Message({ message }) {
+    const demoUserId = 111;
+
+    const isUser = demoUserId === message.userId;
+
+    const ownMessage = isUser ? styles.ownMessage : '';
+
     return (
-        <div className={styles.messageContent + ' ' + styles.ownMessage}>
-            <span>as;kdakl;sda</span>
+        <div className={styles.messageContent + ' ' + ownMessage}>
+            <span>{message.text}</span>
             <div className={styles.messageTriangle}></div>
         </div>
     );
