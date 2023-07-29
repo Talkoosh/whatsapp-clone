83536df85f3fd25a24a67410416af86038868adf
TalSB <artishok.tal@gmail.com>
2023-07-29 04:37:28 +0300

    message text area resized upwards and adds rows

--- src/components/SendMessage/SendMessage.js
+++ src/components/SendMessage/SendMessage.js
@@ -2,21 +2,27 @@ import React, { useRef, useState } from 'react';
 import styles from './sendMessage.module.css';
 
 export function SendMessage() {
-    const textAreaRef = useRef();
+    const containerRef = useRef();
     const [rows, setRows] = useState([]);
 
-    const handleKeyPress = (ev) => {
+    function handleKeyPress(ev) {
         if (ev.keyCode === 13) {
             ev.preventDefault();
             if (ev.shiftKey) {
-                setRows((prevState) => [<input className={styles.messageRow}></input>, ...prevState]);
+                setRows((prevState) => [<input onKeyDown={handleKeyPress} className={styles.messageRow} data-index={prevState.length} />, ...prevState]);
             }
         }
-    };
+        if (ev.keyCode === 8) {
+            const index = ev.target.dataset.index;
+        }
+    }
+
+    const containerHeight = 55 + rows.length * rows[0]?.height;
     return (
-        <section className={styles.container}>
-            <div ref={textAreaRef} onKeyDown={handleKeyPress} contentEditable className={styles.messageText}>
+        <section style={{ height: containerHeight + 'px' }} ref={containerRef} className={styles.container}>
+            <div className={styles.messageTextContainer}>
                 {rows}
+                <input className={styles.messageRow} onKeyDown={handleKeyPress} />
             </div>
         </section>
     );
