926d32ebd0941b707f813fdc6e7ab60f29a7c2e0
TalSB <artishok.tal@gmail.com>
2023-07-29 19:18:17 +0300

    moving up/down rows with arrows functionality in message text area

--- src/components/SendMessage/SendMessage.js
+++ src/components/SendMessage/SendMessage.js
@@ -1,17 +1,36 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './sendMessage.module.css';
 
 export function SendMessage() {
     const containerRef = useRef();
     const [rows, setRows] = useState([]);
+    const [currRowIndex, setCurrRowIndex] = useState(null);
+
+    useEffect(() => {
+        document.querySelector(`[data-index="${currRowIndex}"]`)?.focus();
+    }, [currRowIndex]);
+
+    function setCurrentInput(ev) {
+        const currIndex = Number(ev.target.dataset.index);
+        setCurrRowIndex(currIndex);
+    }
 
     function handleKeyPress(ev) {
+        //on UP/DOWN press
+        if (ev.keyCode === 38) {
+            setCurrRowIndex((prevState) => prevState + 1);
+        }
+        if (ev.keyCode === 40) {
+            setCurrRowIndex((prevState) => prevState - 1);
+        }
+        //On ENTER press
         if (ev.keyCode === 13) {
             ev.preventDefault();
             if (ev.shiftKey) {
-                setRows((prevState) => [<input onKeyDown={handleKeyPress} className={styles.messageRow} data-index={prevState.length} />, ...prevState]);
+                setRows((prevState) => [<input onClick={setCurrentInput} onKeyDown={handleKeyPress} className={styles.messageRow} data-index={prevState.length} />, ...prevState]);
             }
         }
+        //On DELETE press
         if (ev.keyCode === 8) {
             const index = ev.target.dataset.index;
         }
