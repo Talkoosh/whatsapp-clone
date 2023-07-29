6c1b6e14dd05acc61c1028f194ebbf2eec7c9742
TalSB <artishok.tal@gmail.com>
2023-07-29 19:56:56 +0300

    SendMessage input + icons basic design

--- src/components/SendMessage/SendMessage.js
+++ src/components/SendMessage/SendMessage.js
@@ -1,48 +1,23 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styles from './sendMessage.module.css';
-
+import { FaRegLaughBeam } from 'react-icons/fa';
+import { FiPlus } from 'react-icons/fi';
+import { BiSolidMicrophone } from 'react-icons/bi';
+import { RiSendPlane2Fill } from 'react-icons/ri';
 export function SendMessage() {
-    const containerRef = useRef();
-    const [rows, setRows] = useState([]);
-    const [currRowIndex, setCurrRowIndex] = useState(null);
+    const [message, setMessage] = useState(null);
 
-    useEffect(() => {
-        document.querySelector(`[data-index="${currRowIndex}"]`)?.focus();
-    }, [currRowIndex]);
+    const handleInput = (ev) => {
+        const val = ev.target.value;
+        setMessage(val);
+    };
 
-    function setCurrentInput(ev) {
-        const currIndex = Number(ev.target.dataset.index);
-        setCurrRowIndex(currIndex);
-    }
-
-    function handleKeyPress(ev) {
-        //on UP/DOWN press
-        if (ev.keyCode === 38) {
-            setCurrRowIndex((prevState) => prevState + 1);
-        }
-        if (ev.keyCode === 40) {
-            setCurrRowIndex((prevState) => prevState - 1);
-        }
-        //On ENTER press
-        if (ev.keyCode === 13) {
-            ev.preventDefault();
-            if (ev.shiftKey) {
-                setRows((prevState) => [<input onClick={setCurrentInput} onKeyDown={handleKeyPress} className={styles.messageRow} data-index={prevState.length} />, ...prevState]);
-            }
-        }
-        //On DELETE press
-        if (ev.keyCode === 8) {
-            const index = ev.target.dataset.index;
-        }
-    }
-
-    const containerHeight = 55 + rows.length * rows[0]?.height;
     return (
-        <section style={{ height: containerHeight + 'px' }} ref={containerRef} className={styles.container}>
-            <div className={styles.messageTextContainer}>
-                {rows}
-                <input className={styles.messageRow} onKeyDown={handleKeyPress} />
-            </div>
+        <section className={styles.container}>
+            <FaRegLaughBeam className={styles.icon} />
+            <FiPlus className={styles.icon} />
+            <input onChange={handleInput} value={message} placeholder="Type a message" className={styles.messageInput} />
+            {message ? <RiSendPlane2Fill className={styles.icon} /> : <BiSolidMicrophone className={styles.icon} />}
         </section>
     );
 }
