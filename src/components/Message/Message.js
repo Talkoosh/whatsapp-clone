411ed4d8b90f3467dc0262bec7d9c4f9c984ed12
TalSB <artishok.tal@gmail.com>
2023-08-01 20:51:37 +0300

    added chat background and worked on message design. Added sent at below text message

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -8,10 +8,14 @@ export function Message({ message }) {
 
     const ownMessage = isUser ? styles.ownMessage : '';
 
+    const messageSentAt = new Date(message.sentAt);
+    const formattedSentAt = `${messageSentAt.getHours().toString().padStart(2, 0)}:${messageSentAt.getMinutes().toString().padStart(2, 0)}`;
+
     return (
         <div className={styles.messageContent + ' ' + ownMessage}>
             <span>{message.text}</span>
             <div className={styles.messageTriangle}></div>
+            <div className={styles.sentAt}>{formattedSentAt}</div>
         </div>
     );
 }
