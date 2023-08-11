a03e7e9ab006f1b318d3817148b4b937bd29aa9a
TalSB <artishok.tal@gmail.com>
2023-08-12 01:50:53 +0300

    added basic chatlistheader component

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styles from './chatList.module.css';
+import { ChatListHeader } from '../ChatListHeader/ChatListHeader';
 import { ChatPreview } from '../ChatPreview/ChatPreview';
 import demoData from '../../data/fake.json';
 
@@ -12,6 +13,7 @@ export function ChatList() {
 
     return (
         <section className={styles.container}>
+            <ChatListHeader></ChatListHeader>
             {chats?.map((chat) => (
                 <ChatPreview key={chat.id} chat={chat} />
             ))}
---
+++ src/components/ChatListHeader/ChatListHeader.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './chatListHeader.module.css';
+
+export function ChatListHeader() {
+    return <section className={styles.container}></section>;
+}
